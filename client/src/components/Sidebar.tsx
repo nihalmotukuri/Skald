5fd6806795c0171ac60590a2c8ba7aa0d90204e3
Nihal <nihalmotukuri@gmail.com>
2025-07-05 12:24:18 +0530

    updated dashboard and sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,22 +1,29 @@
+import { useSelector } from "react-redux";
 import { TbLayoutDashboard } from "react-icons/tb"
 import { VscTasklist } from "react-icons/vsc"
-import { CiTimer } from "react-icons/ci"
 import { SlNotebook } from "react-icons/sl";
 import { BsLightningChargeFill } from "react-icons/bs";
 
 const features = [
     { name: "Dashboard", icon: <TbLayoutDashboard /> },
     { name: "To-do List", icon: <VscTasklist /> },
-    { name: "Pomodoro", icon: <CiTimer /> },
+    // { name: "Pomodoro", icon: <CiTimer /> },
     { name: "Notes", icon: <SlNotebook /> },
     { name: "Assistant", icon: <BsLightningChargeFill /> }
 ]
 
 const Sidebar = () => {
+    interface RootState {
+        sidebarStore: {
+            value: boolean;
+        };
+    }
+    
+    const open = useSelector((store: RootState) => store.sidebarStore.value)
 
-    return (
+    const openSidebar = () => (
         <div
-            className="bg-[#252a41] min-w-[280px] m-[24px] rounded-[18px] p-[8px] flex flex-col justify-between"
+            className="bg-[#1b1d26] min-w-[280px] m-[24px] rounded-[18px] p-[8px] flex flex-col justify-between"
         >
             <div>
                 <div className="w-[150px] flex justify-center items-center mt-[10px]">
@@ -36,7 +43,7 @@ const Sidebar = () => {
                     {features.map(f => (
                         <li
                             key={f.name}
-                            className="flex items-center gap-3 py-2 px-3 hover:bg-[#2e3350] rounded-lg cursor-pointer text-[18px] my-[10px]">
+                            className="flex items-center gap-3 py-2 px-3 hover:bg-[#0f111a5f] rounded-lg cursor-pointer text-[18px] my-[10px]">
                             <span className="text-[24px]">{f.icon}</span>
                             <span>{f.name}</span>
                         </li>
@@ -44,10 +51,10 @@ const Sidebar = () => {
                 </ul>
             </div>
 
-            <div className="flex items-center hover:bg-[#2e3350] p-[8px] rounded-[12px] cursor-pointer">
-                <img 
+            <div className="flex items-center hover:bg-[#0f111a5f] p-[8px] rounded-[12px] cursor-pointer">
+                <img
                     className="size-[40px] rounded-[10px] mr-[8px]"
-                    src="https://i.pinimg.com/736x/3b/73/48/3b73483fa5af06e3ba35f4f71e541e7a.jpg" 
+                    src="https://i.pinimg.com/736x/3b/73/48/3b73483fa5af06e3ba35f4f71e541e7a.jpg"
                     alt="profile-pic" />
 
                 <div>
@@ -57,6 +64,41 @@ const Sidebar = () => {
             </div>
         </div>
     )
+
+    const closeSidebar = () => (
+        <div
+            className="bg-[#1b1d26] m-[24px] rounded-[12px] flex flex-col justify-between"
+        >
+            <div className='flex flex-col items-center'>
+                <div className="flex justify-center items-center mt-[10px]">
+                    <img
+                        src="/images/logo.png"
+                        className="size-[28px] m-auto logo-animation mx-[16px] mt-[8px]" />
+                </div>
+
+                <ul className="mt-[18px]">
+                    {features.map(f => (
+                        <li
+                            key={f.name}
+                            className="flex items-center gap-3 py-2 px-3 hover:bg-[#0f111a5f] rounded-lg cursor-pointer text-[18px] my-[10px]">
+                            <span className="text-[18px]">{f.icon}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="flex justify-center hover:bg-[#0f111a5f] p-[6px] m-[6px] rounded-[12px] cursor-pointer">
+                <img
+                    className="size-[28px] rounded-[6px]"
+                    src="https://i.pinimg.com/736x/3b/73/48/3b73483fa5af06e3ba35f4f71e541e7a.jpg"
+                    alt="profile-pic" />
+            </div>
+        </div>
+    )
+
+    const renderSidebar = open ? openSidebar() : closeSidebar()
+
+    return renderSidebar
 }
 
 export default Sidebar
